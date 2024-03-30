f7812de4ff3c6da4764ad49934762d3812241aee
nastar <an1413@hs.ac.kr>
2024-03-30 13:26:28 +0900

    Feat: firebase연동

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import app from "./firebase/fBase.js"
+
+console.log(app);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
